8a5b297d1ab7a8e8a39822b42dbcef5eee030524
Vivek Kalyan <hello@vivekkalyan.com>
2025-01-24 00:16:49 +0800

    build: Build web dist into same folder

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -11,8 +11,8 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      pages: "../dist/web",
-      assets: "../dist/web",
+      pages: "./dist/web",
+      assets: "./dist/web",
       fallback: "index.html",
       strict: true,
     }),
