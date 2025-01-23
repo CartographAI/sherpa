08b2839d149a63a61dbaf684fae898a1674ad2b1
Si-Yan Teo <dev@teosiyan.com>
2025-01-23 16:24:16 +0800

    feat: Add toast component from shadcn-svelte (uses svelte-sonner)

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  import { Toaster } from "$lib/components/ui/sonner/index.js";
   import "../app.css";
   let { children } = $props();
 </script>
 
 {@render children()}
+<Toaster />
